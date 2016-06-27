3d302118cbbc28bee158452a3837000f1c54cbed
Jenkins <jenkins@opennodecloud.com>
2016-06-27 19:38:59 +0000

    Preparing new release: 0.3.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ install_requires = [
 
 setup(
     name='nodeconductor-jira',
-    version='0.2.1',
+    version='0.3.0',
     author='OpenNode Team',
     author_email='info@opennodecloud.com',
     url='http://nodeconductor.com',
