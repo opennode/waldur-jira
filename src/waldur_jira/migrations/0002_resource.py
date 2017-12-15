a14ddf1192406cf77e853538fa012ffee8eb2430
Victor Mireyev <victor@opennodecloud.com>
2017-12-15 12:32:05 +0300

    Skip resource content type and object ID validation if it is not specified [WAL-1313]

--- src/waldur_jira/migrations/0002_resource.py
+++ src/waldur_jira/migrations/0002_resource.py
@@ -17,11 +17,11 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='issue',
             name='resource_content_type',
-            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, related_name='jira_issues', to='contenttypes.ContentType'),
+            field=models.ForeignKey(null=True, blank=True, on_delete=django.db.models.deletion.CASCADE, related_name='jira_issues', to='contenttypes.ContentType'),
         ),
         migrations.AddField(
             model_name='issue',
             name='resource_object_id',
-            field=models.PositiveIntegerField(null=True),
+            field=models.PositiveIntegerField(null=True, blank=True),
         ),
     ]
