03d4520881d88a3b4e9172b3a122134809da1f92
Jenkins <jenkins@opennodecloud.com>
2018-05-10 22:14:40 +0000

    Preparing new release: 0.8.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ install_requires = [
 
 setup(
     name='waldur-jira',
-    version='0.7.1',
+    version='0.8.0',
     author='OpenNode Team',
     author_email='info@opennodecloud.com',
     url='http://waldur.com',
